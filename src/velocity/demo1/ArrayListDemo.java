e79524a34cd73f78c406a96e975a0f6e81d62e99
Tanuja-Sabakale <tanuja.sabakale99@gmail.com>
2024-08-08 15:50:48 +0530

    first commit : added arraylist demo code

---
+++ src/velocity/demo1/ArrayListDemo.java
@@ -0,0 +1,15 @@
+package velocity.demo1;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+public static void main(String[] args) {
+	
+	ArrayList<String> al=new ArrayList<String>();
+	al.add("Shayam");
+	al.add("Mohan");
+	al.add("Vinod");
+	
+	System.out.println(al);
+}
+}
